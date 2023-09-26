429cc9388d047e5cbb5cc5407a78f25c1937c479
Titouan Bion <titouan.bion@jetbrains.com>
2023-09-26 20:50:14 +0200

    Working

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -9,16 +9,16 @@ plugins {
 //      > Could not find skiko-awt-runtime-macos-x64-0.7.81.jar (org.jetbrains.skiko:skiko-awt-runtime-macos-x64:0.7.81).
 //       Searched in the following locations:
 //       https://cache-redirector.jetbrains.com/plugins.gradle.org/org/jetbrains/skiko/skiko-awt-runtime-macos-x64/0.7.81/skiko-awt-runtime-macos-x64-0.7.81.jar
-repositories {
-    maven("https://cache-redirector.jetbrains.com/plugins.gradle.org")
-    maven("https://cache-redirector.jetbrains.com/maven.pkg.jetbrains.space/public/p/compose/dev")
-}
+//repositories {
+//    maven("https://cache-redirector.jetbrains.com/plugins.gradle.org")
+//    maven("https://cache-redirector.jetbrains.com/maven.pkg.jetbrains.space/public/p/compose/dev")
+//}
 
 // Without `cache-redirector` it works (to try out, comment out the first `repositories` block and uncomment the following one:
-//repositories {
-//     maven("https://plugins.gradle.org/m2")
-//     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-//}
+repositories {
+     maven("https://plugins.gradle.org/m2")
+     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
 
 val skikos = listOf("macos-arm64", "macos-x64").map { p ->
     val confProvider = configurations.register("skiko_$p") {
